266bd3224835ac975ceb0a735bdca30caf9c7f79
Paul Brewczynski <pbbluesm@gmail.com>
2015-02-14 11:01:11 +0100

    Cleaned dead code. And add MARK's

--- Stanford1/ViewController.swift
+++ Stanford1/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Stored properties
     @IBOutlet weak var display: UILabel!
     var userIsInTheMiddleOfEnteringNumber = false;
+    var stack : Array<Double> = Array<Double>();
     
+    // MARK: Computed properties
+    var displayValue : Double {
+        get {
+            return NSNumberFormatter().numberFromString(self.display.text!)!.doubleValue
+        }
+        set {
+            self.display.text = "\(newValue)"
+        }
+    }
+    
+    
+    // MARK: IBActions
     @IBAction func digitTouchUpInside(sender: UIButton) {
         let digit = sender.currentTitle!
         
@@ -24,7 +38,6 @@ class ViewController: UIViewController {
         }
     }
     
-    var stack : Array<Double> = Array<Double>();
     
     @IBAction func operationTouchUpInside(sender: UIButton) {
         let operationSign = sender.currentTitle!;
@@ -41,35 +54,15 @@ class ViewController: UIViewController {
         case "√": self.performCalculatorOperation() { sqrt($0)}
         default: break
         }
-        
-        /*
-        if(self.stack.count >= 2) {
-            
-            var o1 = self.stack.removeLast()
-            var o2 = self.stack.removeLast()
-            
-            var result :Double  = 0;
-            switch operationSign {
-            case "×": result = self.performOperation(o1, o2:  o2, operation:  {(a1: Double, a2: Double) -> Double in return o1 * o2})
-            case "÷": result = self.performOperation(o1, o2: o2, operation:{ (a1, a2) -> Double in return o2/o1 })
-            case "+": result = self.performOperation(o1, o2: o2, operation: { (a1, a2) in return o1+o2})
-            case "−": result = self.performOperation(o1, o2: o2, operation: { $0-$1})
-                
-            case "√": result = self.performOperation(o1 , operation: { sqrt($0) }) // error bug - extract it and report it (you will feel better)
-                
-            default: break
-            }
-            
-            
-            self.displayValue = result
-            self.stack.append(result)
-            self.userIsInTheMiddleOfEnteringNumber = false
-        }
-*/
     }
     
+    @IBAction func returnTouchUpInside(sender: UIButton) {
+        self.finishEnteringNumber()
+    }
     
+  
     
+    // MARK: Model
     func performCalculatorOperation(operation: (Double, Double) -> Double) { // Two arguments operation
         if(self.stack.count >= 2) {
             self.displayValue = operation(self.stack.removeLast(), self.stack.removeLast())
@@ -77,7 +70,6 @@ class ViewController: UIViewController {
         }
         print("Stack afer operation \(self.stack)")
     }
-    
     
     func performCalculatorOperation(operation: (Double) -> Double) {
         if(self.stack.count >= 1) {
@@ -87,33 +79,9 @@ class ViewController: UIViewController {
         print("Stack afer operation \(self.stack)")
     }
     
-    
-    var displayValue : Double {
-        get {
-            return NSNumberFormatter().numberFromString(self.display.text!)!.doubleValue
-        }
-        set {
-            self.display.text = "\(newValue)"
-        }
-    }
-    
-    
-    func performOperation(o1: Double, o2: Double, operation: (Double, Double) -> Double) -> Double {
-        return operation(o1, o2)
-    }
-    
-    func performOperation(o1: Double,operation:  (Double) -> Double) -> Double {
-        return operation(o1)
-    }
-    
-    @IBAction func returnTouchUpInside(sender: UIButton) {
-        self.finishEnteringNumber()
-    }
-    
     func finishEnteringNumber() {
         self.userIsInTheMiddleOfEnteringNumber = false
         self.stack.append(self.displayValue)
         println("Finished entering number - stack \(stack)")
     }
 }
-
